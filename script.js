22efe61b54d565f72604f2ed6821e6bb71226345
obj-github <140331081+obj-github@users.noreply.github.com>
2024-11-26 11:05:46 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  // Attach a click event to the image
+  $(".menu").click(function() {
+    // Change the image source on click
+    var currentSrc = $(this).attr("src");
+      
+// Toggle between "menu.svg" and "close.svg"
+    var newSrc = currentSrc === "media/menu.svg" ? "media/close.svg" : "media/menu.svg";
+
+    // Change the image source
+    $(this).attr("src", newSrc);
+  });
+});
+
+//Toggle out nav bar
+$(function() {
+    $(".menu").click(function() {
+        $(".nav").toggle(500);
+    });
+}); 
